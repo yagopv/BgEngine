09361765409379c23f2dbcbe2e81b15683421746
Yago Perez Vazquez <yawette@hotmail.com>
2012-04-05 13:30:38 +0200

    v0.4

--- BgEngine.Web/Scripts/jquery.jcrumb.js
+++ BgEngine.Web/Scripts/jquery.jcrumb.js
@@ -20,127 +20,131 @@
 	$('#element').jCrumb(options);
 */
 
-(function($){
-	$.fn.jCrumb = function(options) {
-		var settings = {
-				maxCrumbs : 4,
-				maxMemory : 15,
-				defaultClass : 'jCrumb',
-				jqUI : false,
-				seperator : "&gt;"
-			},
-			getCrumbs = function(){
-				var cookieName = "breadcrumbs=",
+(function ($) {
+    $.fn.jCrumb = function (options) {
+        var settings = {
+            maxCrumbs: 4,
+            maxMemory: 15,
+            defaultClass: 'jCrumb',
+            jqUI: false,
+            seperator: "&gt;"
+        },
+			getCrumbs = function () {
+			    var cookieName = "breadcrumbs=",
 					cookies = document.cookie.split(';');
 
-				for(var i=0;i < cookies.length;i++) {
-					var cookie = cookies[i].replace(/^\s+/,"");
-				
-					if (cookie.indexOf(cookieName) == 0){
-						return cookie.substring(cookieName.length,cookie.length);
-					}
-				}
-				return;
-			},
-			setCrumb = function(crumb){
-				var crumbs = getCrumbs();
+			    for (var i = 0; i < cookies.length; i++) {
+			        var cookie = cookies[i].replace(/^\s+/, "");
 
-				if(crumbs){
-					var maxMemory = settings.maxMemory;
-					crumbCheck = crumbs.split('*');
-
-					if(crumbCheck[crumbCheck.length-1] != crumb){
-						crumbCheck.push(crumb);	
-					}						
-					
-					if(crumbCheck.length > maxMemory){
-						crumbCheck.splice(0,1);
-					}
-					
-					crumbs = crumbCheck.join('*');				
-				}else{
-					crumbs = crumb;
-				}
-				
-				document.cookie = 'breadcrumbs=' + crumbs +'; path=/';
+			        if (cookie.indexOf(cookieName) == 0) {
+			            return cookie.substring(cookieName.length, cookie.length);
+			        }
+			    }
+			    return;
 			},
-			removeCrumb = function(crumb, index){
-				var crumbs = getCrumbs();
-				if(crumbs){
-					crumbCheck = crumbs.split('*');
-					crumbCheck.splice(index,(crumbCheck.length - index));
-					crumbs = crumbCheck.join('*');	
-					document.cookie = 'breadcrumbs=' + crumbs +'; path=/';
-				}
-				
-				return;
+			setCrumb = function (crumb) {
+			    var crumbs = getCrumbs();
+
+			    if (crumbs) {
+			        var maxMemory = settings.maxMemory;
+			        crumbCheck = crumbs.split('*');
+
+			        if (crumbCheck[crumbCheck.length - 1] != crumb) {
+			            crumbCheck.push(crumb);
+			        }
+
+			        if (crumbCheck.length > maxMemory) {
+			            crumbCheck.splice(0, 1);
+			        }
+
+			        crumbs = crumbCheck.join('*');
+			    } else {
+			        crumbs = crumb;
+			    }
+
+			    document.cookie = 'breadcrumbs=' + crumbs + '; path=/';
+			},
+			removeCrumb = function (crumb, index) {
+			    var crumbs = getCrumbs();
+			    if (crumbs) {
+			        crumbCheck = crumbs.split('*');
+			        crumbCheck.splice(index, (crumbCheck.length - index));
+			        crumbs = crumbCheck.join('*');
+			        document.cookie = 'breadcrumbs=' + crumbs + '; path=/';
+			    }
+
+			    return;
 			};
-		return this.each(function() {
-			
-			if (options) { 
-				settings = $.extend(settings, options);
-			}
-	  
-			setCrumb(document.title + '^' +  window.location.href);
-			
-			var crumbs = getCrumbs(),
+        return this.each(function () {
+
+            if (options) {
+                settings = $.extend(settings, options);
+            }
+            if (document.title.length > 30) {
+                setCrumb(document.title.substring(0, 26) + "..." + '^' + window.location.href);
+            }
+            else {
+                setCrumb(document.title + '^' + window.location.href);
+            }            
+
+            var crumbs = getCrumbs(),
 				crumbVal = "",
 				crumbList = $(document.createElement('ul')).addClass(settings.defaultClass).appendTo($(this)),
 				crumbListItem = "";
-						
-			if(settings.jqUI){
-				crumbList.addClass('fg-buttonset fg-buttonset-single ui-helper-clearfix');
-			}
-			
-			if(crumbs){
-				crumbs = crumbs.split('*');
-				startIndex = 0;
-				
-				if(crumbs.length > settings.maxCrumbs){
-					startIndex = crumbs.length - settings.maxCrumbs;
-				}
-				
-				for(var i = startIndex; i < crumbs.length-1;i++){					
-					crumbVal = crumbs[i].split('^');
-					crumbListItem = $(document.createElement('li'));
-					
-					var crumbLink = $(document.createElement('a'));
-						crumbLink.attr({'href' : crumbVal[1]}).text(crumbVal[0]).appendTo(crumbListItem.appendTo(crumbList));
-						crumbLink.click(function(){removeCrumb($(this).text() + '^'+ $(this).attr('href'), $(this).data("index"));});
-						crumbLink.data("index", i);
-					
-					if(settings.jqUI){
-						crumbListItem.addClass('fg-button ui-state-default ui-priority-primary');
-						if(i === 0){
-							crumbListItem.addClass('ui-corner-left');
-							crumbLink.addClass('ui-icon ui-icon-home');
-						}
-						if(startIndex !== 0 && i === startIndex){
-							crumbListItem.addClass('ui-corner-left');
-							crumbLink.addClass('ui-icon ui-icon-carat-1-w');
-						}
-					}else if(settings.seperator){
-						$(document.createElement('span')).html(settings.seperator).appendTo(crumbListItem);
-					}
-				}
-				
-				crumbListItem = $(document.createElement('li'));
-				crumbVal = crumbs[crumbs.length-1].split('^');
-				$(document.createElement('span')).text(crumbVal[0]).appendTo($(crumbListItem).appendTo($(crumbList)));
-				
-				if(settings.jqUI){
-					crumbListItem.addClass('fg-button ui-state-default ui-priority-primary ui-corner-right ui-state-active');
-				}
-				
-				$(".fg-button:not(.ui-state-disabled)").hover(
-					function(){ 
-						$(this).addClass("ui-state-hover"); 
+
+            if (settings.jqUI) {
+                crumbList.addClass('fg-buttonset fg-buttonset-single ui-helper-clearfix');
+            }
+
+            if (crumbs) {
+                crumbs = crumbs.split('*');
+                startIndex = 0;
+
+                if (crumbs.length > settings.maxCrumbs) {
+                    startIndex = crumbs.length - settings.maxCrumbs;
+                }
+
+                for (var i = startIndex; i < crumbs.length - 1; i++) {
+                    crumbVal = crumbs[i].split('^');
+                    crumbListItem = $(document.createElement('li'));
+
+                    var crumbLink = $(document.createElement('a'));
+                    crumbLink.attr({ 'href': crumbVal[1] }).text(crumbVal[0]).appendTo(crumbListItem.appendTo(crumbList));
+                    crumbLink.click(function () { removeCrumb($(this).text() + '^' + $(this).attr('href'), $(this).data("index")); });
+                    crumbLink.data("index", i);
+
+                    if (settings.jqUI) {
+                        crumbListItem.addClass('fg-button ui-state-default ui-priority-primary');
+                        if (i === 0) {
+                            crumbListItem.addClass('ui-corner-left');
+                            crumbLink.addClass('ui-icon ui-icon-home');
+                        }
+                        if (startIndex !== 0 && i === startIndex) {
+                            crumbListItem.addClass('ui-corner-left');
+                            crumbLink.addClass('ui-icon ui-icon-carat-1-w');
+                        }
+                    } else if (settings.seperator) {
+                        $(document.createElement('span')).html(settings.seperator).appendTo(crumbListItem);
+                    }
+                }
+
+                crumbListItem = $(document.createElement('li'));
+                crumbVal = crumbs[crumbs.length - 1].split('^');
+                $(document.createElement('span')).text(crumbVal[0]).appendTo($(crumbListItem).appendTo($(crumbList)));
+
+                if (settings.jqUI) {
+                    crumbListItem.addClass('fg-button ui-state-default ui-priority-primary ui-corner-right ui-state-active');
+                }
+
+                $(".fg-button:not(.ui-state-disabled)").hover(
+					function () {
+					    $(this).addClass("ui-state-hover");
 					},
-					function(){ 
-						$(this).removeClass("ui-state-hover"); 
+					function () {
+					    $(this).removeClass("ui-state-hover");
 					}
 				)
-			}
-		});
-	};
+            }
+        });
+    };
 })(jQuery);
