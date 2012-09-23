d22df7d2d05ea8f9e00f253c0ed1779b82090c39
Yago Perez Vazquez <yawette@hotmail.com>
2012-09-23 20:06:04 +0200

    Added apicontroller

--- BgEngine.Web/Controllers/ApiController.cs
+++ BgEngine.Web/Controllers/ApiController.cs
@@ -15,13 +15,22 @@ namespace BgEngine.Controllers
     public class ApiController : Controller
     {
         private IBlogServices BlogServices;
-        public ApiController(IBlogServices blogservices)
+        private IService<Post> PostServices;
+        private IService<Tag> TagServices;
+        private IService<Category> CategoryServices;
+
+        public ApiController(IBlogServices blogservices, IService<Tag> tagservices, IService<Category> categoryservices, IService<Post> postservices)
         {
             this.BlogServices = blogservices;
+            this.TagServices = tagservices;
+            this.CategoryServices = categoryservices;
+            this.PostServices = postservices;
         }
-        public JsonpResult GetPosts()
+
+        public JsonpResult GetPosts(int? page)
         {
-            System.Collections.Generic.IEnumerable<Post> source = this.BlogServices.HomePostsForRole(false, int.Parse(BgResources.Pager_HomeIndexPostsPerPage));
+            var pageIndex = page ?? 0;            
+            IEnumerable<Post> source = this.PostServices.RetrievePaged(pageIndex, Int32.Parse(BgResources.Pager_HomeIndexPostsPerPage), p => p.DateCreated, false);
             var data =
                 from p in source
                 select new
@@ -37,13 +46,43 @@ namespace BgEngine.Controllers
                 };
             return this.Jsonp(data);
         }
-        public JsonpResult GetPost(int id)
+
+        public JsonpResult GetCategories()
+        {
+            IEnumerable<Category> source = this.CategoryServices.FindAllEntities(null, o => o.OrderBy(c => c.Name) ,null);
+            var data =
+                from c in source
+                select new
+                {
+                    categoryid = c.CategoryId,
+                    name = c.Name,
+                    description = c.Description
+                };            
+            return this.Jsonp(data);
+        }
+
+        public JsonpResult GetTags()
+        {
+            IEnumerable<Tag> source = this.TagServices.FindAllEntities(null, o => o.OrderBy(c => c.TagName), null);
+            var data =
+                from t in source
+                select new
+                {
+                    tagid = t.TagId,
+                    name = t.TagName,
+                    description = t.TagDescription
+                };
+            return this.Jsonp(data);
+        }
+
+        public JsonpResult GetPost(int postid)
         {            
-            Post post = this.BlogServices.FindPost(id);
+            Post post = this.BlogServices.FindPost(postid);
             return this.Jsonp(new
             {
                 title = post.Title,
                 description = post.Description,
+                text = post.Text,
                 commentscount = post.Comments.Count<Comment>(),
                 date = post.DateCreated.ToShortDateString(),
                 category = post.Category.Name,
@@ -64,6 +103,34 @@ namespace BgEngine.Controllers
                         value = r.Value
                     }
             });
+        }
+
+        public JsonpResult GetPostsBy(string by, string id, int? page)
+        {
+            var pageIndex = page ?? 0;
+            IEnumerable<Post> source;
+            if (by == "category")
+            {
+                source = BlogServices.FindPagedPostsByCategory(false, id, pageIndex, 10);
+            }
+            else
+            {
+                source = BlogServices.FindPagedPostsByTag(false, id, pageIndex, 10);
+            }
+            var data =
+                from p in source
+                select new
+                {
+                    postid = p.PostId,
+                    title = p.Title,
+                    description = p.Description,
+                    commentscount = p.Comments.Count<Comment>(),
+                    date = p.DateCreated.ToShortDateString(),
+                    category = p.Category.Name,
+                    thumbnailpath = (p.Image != null) ? p.Image.ThumbnailPath : " ",
+                    user = p.User.Username
+                };
+            return this.Jsonp(data);
         }
     }
 }
